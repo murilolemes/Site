7d2adeef5ee6d9fe9c691aa3fffd6e6188ed8f7f
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2021-07-27 17:37:47 -0300

    adding calling page

---
+++ src/pages/Calls/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+import { Container, Content } from './styles';
+
+const Calls = () => (
+  <Container>
+    <Header />
+    <Content />
+  </Container>
+);
+
+export default Calls;
