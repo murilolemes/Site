15c062b4266fa8e28e15d8215fec5e8c73961d1b
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-03-21 14:10:32 -0300

    mirageJS baseURL api em produção

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  // baseURL: 'http://10.61.20.88:3333',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://mldesenvolvimentoweb.netlify.app/api',
 });
 
 export default api;
